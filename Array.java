50d528df2c4572de21372ee7397a60545e2b3e96
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 14:16:36 +0530

    Array.java

---
+++ Array.java
@@ -0,0 +1,23 @@
+public class Main {
+	public static void main(String[] args) {
+
+		//int [] marks = new int [3];
+		//marks[0] = 89;
+		//marks[1] = 90;
+		//marks[2] = 100;
+
+		String  [] subject = new String[3];
+
+		subject[0] = "guj";
+		subject[1] = "english";
+		subject[2] = "hindi";
+
+		//System.out.println(marks[0]);
+		//System.out.println(marks[1]);
+		//System.out.println(marks[2]);
+
+		for (int i=0; i < 3 ; i++) {
+			System.out.println(subject[i]);
+		}
+	}
+}
