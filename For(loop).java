19a57ccb1c9ad87d1a49b0a046efc509ebb33185
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:34:54 +0530

    For loop.java

---
+++ For(loop).java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		//for loop
+		for (int i = 0; i <= 100; i++) {
+			System.out.println("i love you");
+			//System.out.println("i love my baby");
+		}
+	}
+}
