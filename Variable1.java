302e5366e19c8d89efbee076013d8ff2ec325ce1
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-17 01:50:36 +0530

    Variable1.java

---
+++ Variable1.java
@@ -0,0 +1,30 @@
+package com.apnacollege;
+
+
+public class Main {
+
+
+   public static void main(String[] args) {
+
+      // Variables
+
+       String name = "Aman";
+
+       int age = 30;
+
+
+       String neighbour = "Akku";
+
+       String friend = neighbour;
+       
+       System.out.println(neighbour);
+       
+       System.out.println(friend);
+       
+       System.out.println(age);
+       
+       System.out.println(name);
+
+   }
+
+}
