8bd46ef2e0365efa7f46a3dceb33dbd7b7ecbc2b
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 14:12:16 +0530

    String.equals.java

---
+++ String.equals.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		String name1 = "madhuri";
+		String name2 = "vaghela";
+
+		//if (name1 == name2) {
+			//System.out.println("string is equals");
+		   //} else {
+			//System.out.println("string is not equals");     	
+			//}
+			
+			if (name1.equals(name2)) {
+			System.out.println("string is equals");
+		   } else {
+			System.out.println("string is not equals");     	}
+	}
+}
