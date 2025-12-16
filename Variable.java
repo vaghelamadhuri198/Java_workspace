4f914ceaf325b997b6ada8a9b1801e03c667cc08
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:22:16 +0530

    Variable.java
    A variable in Java is an identifier that refers to a memory location where a value of a specific data type is stored.

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class variable {
+	public static void main(String[] args) {
+		//variable
+		String name = "madhuri";
+		int age = 20;
+		double marks = 200;
+		float price = 400;
+		long number1 = 4689997654467899L;
+		boolean result = false;
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(price);
+		System.out.println(number1);
+		System.out.println(result);
+		System.out.println(a);
+		System.out.println(b);
+		
+  	}
+}
