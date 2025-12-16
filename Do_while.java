56eea249bc5409825179c3301435623cf40f0470
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:36:25 +0530

    Do_while.java

---
+++ Do_while.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		// will loop
+
+		int i = 10;
+		do {
+		//while (i <= 100) {
+			System.out.println(i);
+			//i = i+1;
+			i++;
+		} while (i<=1);
+	}
+}
