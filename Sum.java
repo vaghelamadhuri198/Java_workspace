07680721be3861583ca4a88340bcec254422d0fe
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:26:42 +0530

    Sum.java

---
+++ Sum.java
@@ -0,0 +1,17 @@
+//take a variable a&b and print the sum
+import java.util.*;
+public class Sum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a: ");
+		int a = sc.nextInt();
+
+		System.out.println("Enter a: ");
+		int b = sc.nextInt();
+
+		System.out.println("final result:");
+		int sum = a + b;
+		System.out.print(sum);
+
+	  }
+}
