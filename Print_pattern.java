6f905415f25e1045dc94dec74ff51effe4daeb18
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:25:35 +0530

    Print_pattern.java

---
+++ Print_pattern.java
@@ -0,0 +1,10 @@
+public class pattern {
+	public static void main(String[] args) {
+		//simple print the pattern
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	 }
+}
