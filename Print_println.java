53d20f902ba0c6e003f07669639065b2ad882204
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-16 12:20:15 +0530

    Print_println.java
    print() writes data to the output stream and leaves the cursor at the end of the printed content on the same line.
    
    println()
    println() outputs the specified data to the console and then appends a newline character. This moves the cursor to the beginning of the next line after printing.

---
+++ Print_println.java
@@ -0,0 +1,10 @@
+public class print {
+	public static void main(String[]args) {
+		//print
+		System.out.print("hello");
+		//print ln
+		System.out.println("madhuri");
+		System.out.println("vaghela");
+
+   	}
+}
