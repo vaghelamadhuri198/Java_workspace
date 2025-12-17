1f7d893658c7d5c03a898c1cf13a8d0ba8cbd74b
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2025-12-17 10:53:58 +0530

    While_loop.java

---
+++ While_loop.java
@@ -0,0 +1,18 @@
+//Write an infinite loop using do while condition.
+//public class InfiniteLoopDoWhile {
+//public static void main(String[] args) {
+
+//do {
+//System.out.println("This is an infinite loop!");
+//} while (true);   // Condition always true → infinite loop
+//}
+//}
+
+public class Condition {
+	public static void main(String[]args) {
+		int i = 0;
+		while (i <= 10) {
+			System.out.println("hello");
+		}
+	}
+}
